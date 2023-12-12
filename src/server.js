d4405df475f37667abeed9af52f2a27d0bedc8c6
SatomiJin <satomikoutarou0904Ggmail.com>
2023-12-12 22:40:31 +0700

    fixing

--- src/server.js
+++ src/server.js
@@ -2,30 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import initWebRoute from "./routes/web";
 import * as connectDB from "./config/connectDB";
-import cors from "cors";
+import Cors from "cors";
 import cookieParser from "cookie-parser";
 require("dotenv").config();
 
 let app = express();
-// Add headers before the routes are defined
-// destructor middleware for server
-// app.use(function (req, res, next) {
-//   // Website you wish to allow to connect
-//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
-
-//   // Request methods you wish to allow
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-
-//   // Request headers you wish to allow
-//   res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
-
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-
-//   // Pass to next layer of middleware
-//   next();
-// });
 
 // app.use((req, res, next) => {
 //   res.header("Access-Control-Allow-Origin", `https://ecommerce-v2-fe.vercel.app/`);
