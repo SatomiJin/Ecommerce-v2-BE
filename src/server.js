fbafe8c855f9e730c36f6d9c4775ac44045d6023
SatomiJin <satomikoutarou0904Ggmail.com>
2023-12-12 22:14:08 +0700

    lần 2

--- src/server.js
+++ src/server.js
@@ -28,7 +28,7 @@ let app = express();
 // });
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", `${process.env.URL_REACT}`);
+  res.header("Access-Control-Allow-Origin", `https://ecommerce-v2-fe.vercel.app/`);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
